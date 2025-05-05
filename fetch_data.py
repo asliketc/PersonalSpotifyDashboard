3ced9d0fa3e96e09882066c3e439a86c2bd07390
a <a>
2025-05-05 14:29:38 +0300

    changes in fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -4,6 +4,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth #for secure user authentication
 import pandas as pd 
 import json 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
 
 #-------STEP 1: Loading environmental variables------
 load_dotenv()
@@ -39,26 +41,25 @@ track_data=[]
 for item in recent['items']:
 
     track=item['track']
-    track_id=track['id']
-
-    #getting audio features
-    try:
-        features_list = sp.audio_features([track_id])
-        features = features_list[0] if features_list else None
-        if features is None:
-            print(f"⚠️ No audio features found for track ID: {track_id}")
-            continue
-    except Exception as e:
-        print(f"❌ Failed to get features for {track['name']} ({track_id}): {e}")
-        continue
- #returns a list so we get [0]
-
-    #artist & genre information
-    artist_id=track['artists'][0]['id']
-    artist_info=sp.artist(artist_id)
-    genres=artist_info['genres']
-    genre= genres[0] if genres else "unknown genre"
+    artist=track['artists'][0]
+    artist_info=sp.artist(artist['id'])
+    genre=artist_info['genres'][0] if artist_info['genres'] else "unknown"
     #there can be more than one genre so takes the FIRST genre information
+
+#     #getting audio features
+#     try:
+#         features_list = sp.audio_features([track_id])
+#         features = features_list[0] if features_list else None
+#         if features is None:
+#             print(f"⚠️ No audio features found for track ID: {track_id}")
+#             continue
+#     except Exception as e:
+#         print(f"❌ Failed to get features for {track['name']} ({track_id}): {e}")
+#         continue
+#  #returns a list so we get [0]
+
+    
+   
 
     track_data.append({
         'played_at':item['played_at'],
@@ -68,13 +69,7 @@ for item in recent['items']:
         'popularity':track['popularity'],
         'duration_ms':track['duration_ms'],
         'genre': genre,
-        'danceability':features['danceability'],
-        'energy':features['energy'],
-        'valence':features['valence'],
-        'tempo':features['tempo'],
-        'acousticness':features['acousticness'],
-        'speechiness':features['speechiness'],
-        'loudness':features['loudness']
+        'release_year':int(track['album']['release_date'][:4])
     })
 
 
@@ -83,31 +78,17 @@ df=pd.DataFrame(track_data)
 df['played_at']=pd.to_datetime(df['played_at'])
 df['weekday']=df['played_at'].dt.day_name()
 df['hour']=df['played_at'].dt.hour
-df['month_name']=df['played_at'].dt.month_name
 
-    #season retrieval
-def season_info(month):
-    if month in [12,1,2]:
-        return "Winter"
-    elif month in [3,4,5]:
-        return "Spring"
-    elif month in [6,7,8]:
-        return "Summer"
-    elif month in [9,10,11]:
-        return "Fall"
-    else:
-        return "Unable to detect"
-        
-df['season']=df['month'].apply(season_info)
+#print(df)
 
+# #-----STEP 4: TO CSV-----
+# os.makedirs("data",exist_ok=True)
+# df.to_csv("data/recentTracks.csv",index=False)
+# print("Data Saved to new .csv")
 
-
-
-#-----STEP 4: TO CSV-----
-os.makedirs("data",exist_ok=True)
-df.to_csv("data/features_of_recenttracks.csv",index=False)
-print("Data Saved to new .csv")
-
-
-
-
+plt.figure(figsize=(10,5))
+sns.countplot(data=df, x='genre',order=df['genre'].value_counts().index[:10])
+plt.xticks(rotation=45)
+plt.title("Top Genres")
+plt.tight_layout()
+plt.show()
